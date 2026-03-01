ad7a1023395c18c51a6aed65f21b6ad5293cd895
Bansi Javiya <bansijaviya73@gmail.com>
2026-03-01 19:44:43 +0530

    Updated model logic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 import joblib
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import os
 
 app = Flask(__name__, static_folder="Frontend", static_url_path="")
